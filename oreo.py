9dffc0472074326b44e329f122bbdaed3d4ea640
JoeyShapiro <joeyashapiro@gmail.com>
2023-07-07 13:02:16 -0500

    webhook gets api stuff

--- oreo.py
+++ oreo.py
@@ -19,19 +19,30 @@ def event_streamup():
     data = request.json # type: ignore
 
     channel = data["event"]["broadcaster_user_name"]
+
+    # get the stream info, like title and stuff
+    auth = get_auth(secrets['twitch']['client_id'], secrets['twitch']['client_secret'])
+    details = get_stream('141981764', auth['access_token'], secrets['twitch']['client_id']) # type: ignore
+
     conn = sqlite3.connect('oreo.sqlite')
 
     c = conn.cursor()
-    c.execute(f'SELECT channel, message, p.name FROM hooks INNER JOIN platforms p on hooks.platform_id = p.id WHERE channel = "{channel}"')
+    c.execute(f'''
+            SELECT channel, message, p.name FROM hooks
+                INNER JOIN platforms p on hooks.platform_id = p.id 
+                WHERE channel = "{channel}" AND platform_id = 2
+            ''') # do i need the name, i know the platform anyway from the call. for later function collection stuff
 
     # fetch the results
     results = c.fetchall()
 
-    # print the results
+    # send it to each subscription with the proper message
     for row in results:
         msg = row[1]
         msg = msg.replace('{channel}', row[0]).replace('{platform}', row[2])
         msg = msg.replace('{link}', f'https://twitch.tv/{channel}')
+        msg = msg.replace('{title}', details['title'])
+        msg = msg.replace('{game}', details['game_name']) # maybe change to category        
 
         discord_message = {
             "content": msg
@@ -73,8 +84,4 @@ def get_stream(broadcaster_id: str, bearer_token: str, client_id: str):
     return res.json()['data'][0] # this is a function, get the only value
 
 if __name__ == "__main__":
-    auth = get_auth(secrets['twitch']['client_id'], secrets['twitch']['client_secret'])
-    print(auth)
-    details = get_stream('141981764', auth['access_token'], secrets['twitch']['client_id']) # type: ignore
-    print(details['title'])
     app.run(debug=True, host="0.0.0.0", port=8080)
